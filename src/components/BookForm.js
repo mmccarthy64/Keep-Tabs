de1c9394c4689c09c03e333ed571efd289f53ae7
mmccarthy64 <mmccarthy64@hotmail.com>
2021-06-25 18:55:52 -0500

    Update Book Form

--- src/components/BookForm.js
+++ src/components/BookForm.js
@@ -4,14 +4,33 @@ class BookForm extends Component {
     render() {
         return (
             <div id='book-form'>
-                <form>
+                <form onSubmit={this.handleOnSubmit}>
                     <div>
-                        <label for='title' />
-                        <input type='text' />
+                        <label htmlFor='title'>Title</label>
+                        <div>
+                            <input type='text' />
+                        </div>
                     </div>
                     <div>
-                        <label />
-                        <input />
+                        <label htmlFor='author'>Author</label>
+                        <div>
+                            <input
+                                type='text'
+                                
+                                />
+                        </div>
+                    </div>
+                    <div>
+                        <label htmlFor='title'>Page Count</label>
+                        <div>
+                            <input type='text' />
+                        </div>
+                    </div>
+                    <div>
+                        <label htmlFor='image'>Image URL</label>
+                        <div>
+                            <input type='text' />
+                        </div>
                     </div>
                 </form>
             </div>
