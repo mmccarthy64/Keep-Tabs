d660f8c7b923d5b73e4c6258ecd077768c2d63c6
mmccarthy64 <mmccarthy64@hotmail.com>
2021-06-25 19:01:07 -0500

    Add book form inputs

--- src/components/BookForm.js
+++ src/components/BookForm.js
@@ -1,6 +1,25 @@
 import React, { Component } from 'react';
 
 class BookForm extends Component {
+
+    state = {
+        title: '',
+        author: '',
+        page_count: 0,
+        image: ''
+    }
+
+    handleOnSubmit = e => {
+        e.preventDefault()
+        this.setState({
+
+        })
+    }
+
+    handleOnChange = e => {
+
+    }
+
     render() {
         return (
             <div id='book-form'>
@@ -8,7 +27,11 @@ class BookForm extends Component {
                     <div>
                         <label htmlFor='title'>Title</label>
                         <div>
-                            <input type='text' />
+                            <input 
+                                type='text'
+                                name='title'
+                                value={this.state.title}
+                                onChange={this.handleOnChange} />
                         </div>
                     </div>
                     <div>
@@ -16,21 +39,34 @@ class BookForm extends Component {
                         <div>
                             <input
                                 type='text'
-                                
+                                name='author'
+                                value={this.state.author}
+                                onChange={this.handleOnChange}
                                 />
                         </div>
                     </div>
                     <div>
-                        <label htmlFor='title'>Page Count</label>
+                        <label htmlFor='page_count'>Page Count</label>
                         <div>
-                            <input type='text' />
+                            <input
+                                type='text'
+                                name='page_count'
+                                value={this.state.page_count}
+                                onChange={this.handleOnChange} />
                         </div>
                     </div>
                     <div>
                         <label htmlFor='image'>Image URL</label>
                         <div>
-                            <input type='text' />
+                            <input
+                                type='text'
+                                name='image'
+                                valu={this.state.image}
+                                onChange={this.handleOnChange} />
                         </div>
+                    </div>
+                    <div>
+                        <button type='submit' />
                     </div>
                 </form>
             </div>
