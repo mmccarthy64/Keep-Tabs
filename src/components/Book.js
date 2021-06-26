655595c1e0710175afa3d8d3bd7d2ec328145ce9
mmccarthy64 <mmccarthy64@hotmail.com>
2021-06-26 14:13:19 -0500

    Add props from Books to Book

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 
 class Book extends Component {
+ 
     render() {
         return (
             <div>
-                Each Book
+                <h1>{this.props.title}</h1>
+                <img src={this.props.image} alt='book-image' />
             </div>
         );
     }
--- src/components/Books.js
+++ src/components/Books.js
@@ -3,7 +3,13 @@ import Book from './Book';
 
 class Books extends Component {
     render() {
-        const books = this.props.books.map( book => <Book />)
+        const books = this.props.books.map( book => 
+            <Book key={book.id}
+                title={book.attributes.title}
+                author={book.attributes.author}
+                page_count={book.attributes.page_count}
+                image={book.attributes.image}
+            />)
         return (
             <div>
                 <h1>Library</h1>
