0ac3d5e90c1d3d5ce31b9b39c00409cbd3ae1442
mmccarthy64 <mmccarthy64@hotmail.com>
2021-06-26 15:04:02 -0500

    Add card styling to Books

--- src/components/Book.js
+++ src/components/Book.js
@@ -6,10 +6,21 @@ class Book extends Component {
         const { title, author, page_count, image } = this.props
         return (
             <div>
-                <h1>{title}</h1>
-                <p>{author}</p>
-                <p>{page_count}</p>
-                <img src={image} alt='book-cover' />
+
+                <div className="card card-inverse card-success card-primary mb-3 text-center">
+                    <div className="card-block">
+                        <blockquote className="card-blockquote">
+                        <h2>{title}</h2>
+                        <p>{page_count}</p>
+                        <img src={image} alt='book-cover' />
+                        <footer>- author <cite title="Source Title">{author}</cite></footer>
+                        </blockquote>
+                    </div>
+                    <div className="float-right">
+                        <div className="btn-group btn-group-sm" role="group" aria-label="Basic example">
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
