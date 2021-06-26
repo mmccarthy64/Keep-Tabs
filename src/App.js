b9072bb0797429f3075cc65da0f1a6096c918625
mmccarthy64 <mmccarthy64@hotmail.com>
2021-06-26 12:14:00 -0500

    Add Switch from react-router

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import BooksContainer from "./containers/BooksContainer";
 import NavBar from "./components/NavBar";
 import {
   BrowserRouter as Router,
-  Switch, 
-  Route
 } from "react-router-dom";
 
 function App() {
--- src/containers/BooksContainer.js
+++ src/containers/BooksContainer.js
@@ -1,19 +1,23 @@
 import React, { Component } from 'react';
 import BookForm from '../components/BookForm';
 import Books from '../components/Books'
-import { Route } from 'react-router';
+import { Route, Switch } from 'react-router';
 
 class BooksContainer extends Component {
     render() {
         return (
             <div>
-                <Route exact path='/library'>
-                    < Books />
-                </Route>
-                <Route exact path='/new'>
-                    < BookForm />
-                </Route>
-                {/* search GoogleBooks */}
+                <Switch>
+                    <Route exact path='/library'>
+                        < Books />
+                    </Route>
+                    <Route exact path='/new'>
+                        < BookForm />
+                    </Route>
+                    <Route exact path='/search'>
+                        {/* search GoogleBooks */}
+                    </Route>
+                </Switch>
             </div>
         );
     }
