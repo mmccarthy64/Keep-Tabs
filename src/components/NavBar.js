4682fbffe7daff38f22ccff85b113ef2fb54663c
mmccarthy64 <mmccarthy64@hotmail.com>
2021-06-28 11:01:01 -0500

    Update Nav links

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Link } from "react-router-dom";
-import { Navbar} from 'react-bootstrap';
+import { Nav, Navbar} from 'react-bootstrap';
 
 const NavBar = () => {
     return (
         <Navbar bg="dark" variant="dark">
             <Navbar.Brand><h2>Keep Tabs</h2></Navbar.Brand>
-                <Link to='/library'><h2>Library</h2></Link>
-                <Link to='/books/new'><h2>Add New Book</h2></Link>
-                <Link to='/search'><h2>Search</h2></Link>
+                <Nav.Link as={Link} to='/library'><h2>My Library</h2></Nav.Link>
+                <Nav.Link as={Link} to='/books/new'><h2>Add Book</h2></Nav.Link>
+                <Nav.Link as={Link} to='/search'><h2>Search Books</h2></Nav.Link>
         </Navbar>
     );
 }
