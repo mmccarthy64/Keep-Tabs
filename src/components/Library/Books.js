a57043286dbac9439bf753e21fc022e401f91ad5
mmccarthy64 <mmccarthy64@hotmail.com>
2021-07-01 10:07:54 -0500

    Add library search function

--- src/components/Library/Books.js
+++ src/components/Library/Books.js
@@ -1,30 +1,37 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Book from './Book';
 
-
-class Books extends Component {
-    render() {
-        const books = this.props.books.map( book => 
-            <Book key={book.id}
-                id={book.id}
-                title={book.title}
-                author={book.author}
-                page_count={book.page_count}
-                image={book.image}
-                comments={this.props.comments}
-                getComments={this.props.getComments}
-                hideComments={this.props.hideComments}
-                deleteBook={this.props.deleteBook}
-            />)
-        return (
-            <div style={{ textAlign: 'center' }}>
-                <h1>My Library</h1>
-                <div className='grid'>
-                    {books}
-                </div>
+const Books = (props) => {
+    const [searchTerm, setSearchTerm] = useState('')
+    const books = props.books.filter((book) => {
+        if (searchTerm === ''){
+            return book
+        } else if ( book.title.toLowerCase().includes(searchTerm.toLowerCase()) || book.author.toLowerCase().includes(searchTerm.toLowerCase())){
+            return book
+        }
+    }).map( book => 
+        <Book key={book.id}
+            id={book.id}
+            title={book.title}
+            author={book.author}
+            page_count={book.page_count}
+            image={book.image}
+            comments={props.comments}
+            getComments={props.getComments}
+            hideComments={props.hideComments}
+            deleteBook={props.deleteBook}
+        />)
+    return (
+        <div style={{ textAlign: 'center' }}>
+            <h1>My Library</h1>
+            <div>
+                <input type='text' placeholder='Search titles...' onChange={ (event) => setSearchTerm(event.target.value)} />
             </div>
-        );
-    }
+            <div className='grid'>
+                {books}
+            </div>
+        </div>
+    );
 }
 
 export default Books;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,15 @@
-import React from 'react';
 import { Link } from "react-router-dom";
 import { Nav, Navbar} from 'react-bootstrap';
 
 const NavBar = () => {
     return (
         <Navbar bg="dark" variant="dark">
-            <Navbar.Brand><h2>Keep Tabs</h2></Navbar.Brand>
-                <Nav.Link as={Link} to='/library'><h2>My Library</h2></Nav.Link>
-                <Nav.Link as={Link} to='/books/new'><h2>Add Book</h2></Nav.Link>
-                <Nav.Link as={Link} to='/search'><h2>Search Books</h2></Nav.Link>
+            <Nav>
+                <Navbar.Brand><h2>Keep Tabs</h2></Navbar.Brand>
+                    <Nav.Link as={Link} to='/library'><h2>My Library</h2></Nav.Link>
+                    <Nav.Link as={Link} to='/books/new'><h2>Add Book</h2></Nav.Link>
+                    <Nav.Link as={Link} to='/search'><h2>Search Google Books</h2></Nav.Link>
+            </Nav>
         </Navbar>
     );
 }
