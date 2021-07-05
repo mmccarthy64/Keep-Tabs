27702cc8f15e6a9972a258c6e5e48a470adfacc4
mmccarthy64 <mmccarthy64@hotmail.com>
2021-07-05 15:43:25 -0500

    create dashboard component as specific store to user

--- src/App.js
+++ src/App.js
@@ -1,61 +1,20 @@
+import React, { Component } from 'react';
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware, compose } from 'redux'
+import manageLibrary from './reducers/manageLibrary'
+import thunk from 'redux-thunk';
+import Dashboard from './Dashboard';
 
-import NavBar from "./components/NavBar";
-import {
-  Route,
-  Switch,
-  BrowserRouter as Router,
-} from "react-router-dom";
-import { Component } from "react";
-import { connect } from "react-redux";
-import { createBook, deleteBook, getBooks, loadGoogleResults, fetchComments, hideComments } from './actions/books';
-import Books from "./components/Library/Books";
-import BookForm from "./components/Library/BookForm";
-import SearchContainer from "./containers/SearchContainer";
+const store = createStore(manageLibrary, compose(applyMiddleware(thunk),window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()))
 
-class App extends Component{
-
-  componentDidMount(){
-    this.props.getBooks()
-  }
-  render(){
-    return (
-      <div className="App">
-        < Router >
-          <NavBar />
-          <Switch>
-              <Route path='/library'>
-                  < Books 
-                  books={this.props.books}
-                  deleteBook={this.props.deleteBook}
-                  comments={this.props.comments}
-                  getComments={this.props.getComments}
-                  hideComments={this.props.hideComments}/>
-              </Route>
-              <Route path='/search'>
-                  <SearchContainer 
-                  search={this.props.search}
-                  searchResults={this.props.searchResults}
-                  createBook={this.props.createBook}/>
-              </Route>
-              <Route path='/books/new'>
-                  < BookForm createBook={this.props.createBook} />
-              </Route>
-          </Switch>
-        </Router>
-      </div>
-    );
-  }
+class App extends Component {
+    render() {
+        return (
+            <Provider store={store}>
+                <Dashboard />
+            </Provider>
+        );
+    }
 }
 
-const mapStateToProps = state => ({ books: state.books, searchResults: state.searchResults, comments: state.comments })
-
-const mapDispatchToProps = dispatch => ({
-  createBook: book => dispatch(createBook(book)),
-  getBooks: books => dispatch(getBooks(books)),
-  deleteBook: id => dispatch(deleteBook(id)),
-  search: payload => dispatch(loadGoogleResults(payload)),
-  getComments: id => dispatch(fetchComments(id)),
-  hideComments: id => dispatch(hideComments(id))
-})
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default App;
---
+++ src/Dashboard.js
@@ -0,0 +1,60 @@
+import NavBar from "./components/NavBar";
+import {
+  Route,
+  Switch,
+  BrowserRouter as Router,
+} from "react-router-dom";
+import { Component } from "react";
+import { connect } from "react-redux";
+import { createBook, deleteBook, getBooks, loadGoogleResults, fetchComments, hideComments } from './actions/books';
+import Books from "./components/Library/Books";
+import BookForm from "./components/Library/BookForm";
+import SearchContainer from "./containers/SearchContainer";
+
+class Dashboard extends Component{
+
+  componentDidMount(){
+    this.props.getBooks()
+  }
+  render(){
+    return (
+      <div className="Dashboard">
+        < Router >
+          <NavBar />
+          <Switch>
+              <Route path='/library'>
+                  < Books 
+                  books={this.props.books}
+                  deleteBook={this.props.deleteBook}
+                  comments={this.props.comments}
+                  getComments={this.props.getComments}
+                  hideComments={this.props.hideComments}/>
+              </Route>
+              <Route path='/search'>
+                  <SearchContainer 
+                  search={this.props.search}
+                  searchResults={this.props.searchResults}
+                  createBook={this.props.createBook}/>
+              </Route>
+              <Route path='/books/new'>
+                  < BookForm createBook={this.props.createBook} />
+              </Route>
+          </Switch>
+        </Router>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => ({ books: state.books, searchResults: state.searchResults, comments: state.comments })
+
+const mapDispatchToProps = dispatch => ({
+  createBook: book => dispatch(createBook(book)),
+  getBooks: books => dispatch(getBooks(books)),
+  deleteBook: id => dispatch(deleteBook(id)),
+  search: payload => dispatch(loadGoogleResults(payload)),
+  getComments: id => dispatch(fetchComments(id)),
+  hideComments: id => dispatch(hideComments(id))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Dashboard);
