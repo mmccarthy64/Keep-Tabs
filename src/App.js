bdb532820f3821765c214f6db3de88fd7304c277
mmccarthy64 <mmccarthy64@hotmail.com>
2021-07-05 15:55:35 -0500

    create store in app instead of index to be user specific

--- src/App.js
+++ src/App.js
@@ -4,15 +4,27 @@ import { createStore, applyMiddleware, compose } from 'redux'
 import manageLibrary from './reducers/manageLibrary'
 import thunk from 'redux-thunk';
 import Dashboard from './Dashboard';
+import {
+    Route,
+    Switch,
+    BrowserRouter,
+  } from "react-router-dom";
 
 const store = createStore(manageLibrary, compose(applyMiddleware(thunk),window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()))
 
 class App extends Component {
     render() {
         return (
-            <Provider store={store}>
-                <Dashboard />
-            </Provider>
+            <BrowserRouter>
+            <Switch>
+                <Route exact path='/' />
+                <Route exact path='/dashboard'>
+                    <Provider store={store}>
+                        <Dashboard />
+                    </Provider>
+                </Route>
+            </Switch>
+            </BrowserRouter>
         );
     }
 }
